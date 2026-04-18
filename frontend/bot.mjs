af391ef20852096f613d58fb218a944b25a76bff
Екатерина <ekaterina@MacBook-Air-Ekaterina.local>
2026-04-18 07:13:05 +0300

    dkjh

--- frontend/bot.mjs
+++ frontend/bot.mjs
@@ -1,10 +1,20 @@
-const { ethers } = require("ethers");
+import { readFile } from "node:fs/promises";
+import { ethers } from "ethers";
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL || "http://127.0.0.1:8545");
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const provider = new ethers.JsonRpcProvider( "http://127.0.0.1:8545");
+
+const keystorePath = process.env.KEYSTORE_PATH;//полный путь до пароля
+const accountPassword = process.env.ACCOUNT_PASSWORD;
+
+const keystoreJson = await readFile(keystorePath, "utf8");
+const baseWallet = await ethers.Wallet.fromEncryptedJson(keystoreJson, accountPassword);
+const wallet = baseWallet.connect(provider);
+//function getBorrowers() external view returns (address[] memory) {
+//     return borrowers;
+// }
 
 const ABI = [
-    "function borrowers(uint) view returns (address)",
+    "function borrowers(uint256) view returns (address)",
     "function _isHealthy(address) view returns (bool)",
     "function liquidate(address) external",
 ];
@@ -17,15 +27,27 @@ const MARKETS = [
 
 async function run() {
     for (const addr of MARKETS) {
-        const m = new ethers.Contract(addr, ABI, wallet);
-        for (let i = 0; ; i++) {
-            let b;
-            try { b = await m.borrowers(i); } catch { break; }
-            if (!(await m._isHealthy(b))) await (await m.liquidate(b)).wait();
+        const market = new ethers.Contract(addr, ABI, wallet);
+
+        for (let i = 0;  i+=1;) {
+            let borrower;
+            try {
+                borrower = await market.borrowers(i);
+            } catch {
+                break;
+            }
+
+            const isHealthy = await market._isHealthy(borrower);
+            if (isHealthy) {
+                continue;
+            }
+
+            const tx = await market.liquidate(borrower);
+            await tx.wait();
         }
     }
 }
 
 console.log("бот запущен");
-run();
+await run();
 setInterval(run, 10 * 60 * 1000);
